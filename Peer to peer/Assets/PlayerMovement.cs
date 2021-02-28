73fa065d5e13bfbbe3f56050c2873491593f6d35
Mark-42-max <79685489+Mark-42-max@users.noreply.github.com>
2021-02-28 18:09:30 +0530

    Move&Gravity

--- Peer to peer/Assets/PlayerMovement.cs
+++ Peer to peer/Assets/PlayerMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController controller;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    readonly float gravity = -9.8f;
+
+    readonly float speed = 12f;
+
+    readonly float groundRad = 0.4f;
+
+    bool isGrounded;
+    Vector3 velocity;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundRad, groundLayer);
+        if(isGrounded && velocity.y<0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Vertical");
+        float z = Input.GetAxis("Horizontal");
+
+        Vector3 move = transform.right * z + transform.forward * x;
+        controller.Move(move *speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
